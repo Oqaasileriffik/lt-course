13164db059cd06b29459edad4d53ed966b3dbc66
Tino Didriksen <mail@tinodidriksen.com>
2020-02-11 16:29:05 +0100

    macOS += python38

--- lecture01/scripts/install-macos.sh
+++ lecture01/scripts/install-macos.sh
@@ -24,11 +24,13 @@ sudo port uninstall inactive
 
 echo ""
 echo "Installing required ports"
-for PKG in subversion git pkgconfig autoconf automake gawk xz perl5 p5-file-homedir p5-ipc-run p5-app-cpanminus p5-plack p5-yaml-libyaml p5-json p5-json-xs timeout
+for PKG in subversion git pkgconfig autoconf automake gawk xz perl5 p5-file-homedir p5-ipc-run p5-app-cpanminus p5-plack p5-yaml-libyaml p5-json p5-json-xs timeout python38
 do
 	echo "... installing $PKG"
 	sudo port install "$PKG" || echo "FAILED TO INSTALL $PKG"
 done
+
+sudo port select --set python3 python38
 
 export GIELLA_CORE=~/langtech/giella-core
 export GIELLA_SHARED=~/langtech/giella-shared
